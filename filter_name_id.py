fdcf778d54e28c56f19bde4ff61af59b36ac4397
K_B <85124305+Velvetbin@users.noreply.github.com>
2022-01-12 00:27:48 +0700

    fix

--- filter_name_id.py
+++ filter_name_id.py
@@ -16,44 +16,51 @@ with open("products.json") as json_file:
 def filter_by_name():
     # This function takes string as an input to spot the string input in the products list
     # It will return a list of products that have the word written in the string
-
-    search_input = input("Please enter the name of the item you want to find: \n>")
-    item_list = []
-    for i in range(0, len(lst_products)):
-        match_keyword = re.findall(search_input, lst_products[i].get('name'))
-        if match_keyword:
-            item_list.append(lst_products[i].get('name'))
+    stopword = 'y'
+    while stopword == 'y':
+        search_input = input("Please enter the name of the item you want to find: \n>")
+        item_list = []
+        for i in range(0, len(lst_products)):
+            match_keyword = re.findall(search_input, lst_products[i].get('name'))
+            if match_keyword:
+                item_list.append(lst_products[i].get('name'))
+            else:
+                pass
+        print("Here is the result of your search:")
+        if not item_list:
+            print("None")
         else:
-            pass
-    print("Here is the result of your search:")
-    if not item_list:
-        print("None")
-    else:
-        for num in range(0, len(item_list)):
-            print('{}.'.format(num+1) + item_list[num])
-
+            for num in range(0, len(item_list)):
+                print('{}.'.format(num+1) + item_list[num])
+        while True:
+            stopword = input("Do you want to continue searching? [y/n] \n>")
+            if stopword in ['y','n']:
+                break
 
 
 # filter items by id
 def filter_by_id():
     # This function takes string as an input to spot the string input in the products list
     # It will return a list of products containing the id
-
-    search_input = input("Please enter the id of the item you want to find: \n>")
-    item_list = []
-    for i in range(0, len(lst_products)):
-        match_keyword = re.findall(search_input, lst_products[i].get('id'))
-        if match_keyword:
-            word_added = "[{}] {}".format(lst_products[i].get('id'), lst_products[i].get('name'))
-            item_list.append(word_added)
+    stopword = 'y'
+    while stopword == 'y':
+        search_input = input("Please enter the id of the item you want to find: \n>")
+        item_list = []
+        for i in range(0, len(lst_products)):
+            match_keyword = re.findall(search_input, lst_products[i].get('id'))
+            if match_keyword:
+                word_added = "[{}] {}".format(lst_products[i].get('id'), lst_products[i].get('name'))
+                item_list.append(word_added)
+            else:
+                pass
+        print("Here is the result of your search:")
+        if not item_list:
+            print("None")
         else:
-            pass
-    print("Here is the result of your search:")
-    if not item_list:
-        print("None")
-    else:
-        for num in range(0, len(item_list)):
-            print('{}. {}'.format(num+1, item_list[num]))
+            for num in range(0, len(item_list)):
+                print('{}. {}'.format(num+1, item_list[num]))
+        while True:
+            stopword = input("Do you want to continue searching? [y/n] \n>")
+            if stopword in ['y','n']:
+                break
 
-
-filter_by_id()
